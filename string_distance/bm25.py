9b766116a7fa193f4878fa060b9d94d4beb611ff
Brian Lester <blester125@gmail.com>
2018-05-12 17:40:27 -0400

    update bm25

--- string_distance/bm25.py
+++ string_distance/bm25.py
@@ -21,21 +21,18 @@ def BM25(corpus, k=1.5, b=0.75):
         # This is the weird IDF that BM25 uses
         idf[token] = math.log((corpus_size - freq + 0.5) / (freq + 0.5))
 
-    avg_idf = sum(idf_ for idf_ in idf.values()) / len(idf)
-    corpus = list(map(set, corpus))
+    avg_idf = sum(x for x in idf.values()) / len(idf)
 
     def score(query, d):
         if isinstance(d, int):
-            text = corpus[d]
             freq = freqs[d]
             dlen = doc_len[d]
         else:
-            text = set(d)
             freq = Counter(d)
             dlen = len(d)
         val = 0
         for token in query:
-            if token not in text:
+            if token not in freq:
                 continue
             idf_ = idf[token] if idf[token] >= 0 else epsilon * avg_idf
             val += idf_ * (freq[token] * (k + 1)) / (freq[token] + k * (1 - b + b * (dlen / avg_len)))
