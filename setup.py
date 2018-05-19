2e7410a8d19d9e5e7d4c04a3e79eed197e3f5476
Brian Lester <blester125@gmail.com>
2018-05-19 17:20:50 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ import re
 from setuptools import setup, find_packages, Extension
 
 def get_version(project_name):
-    regex = re.compile(r"^__version__ = '(\d+\.\d+\.\d+)'$")
-    with open("{}/__init__.py".format(project_name)) as f:
+    regex = re.compile(r"^__version__ = '(\d+\.\d+\.\d+(?:a|b|rc)?(?:\d)*?)'$")
+    with open(f"{project_name}/__init__.py") as f:
         for line in f:
             m = regex.match(line)
             if m is not None:
                 return m.groups(1)[0]
+
 
 def convert_images(text):
     image_regex = re.compile(r"!\[(.*?)\]\((.*?)\)")
